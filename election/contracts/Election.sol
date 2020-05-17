490493c5b5af8c33701c16c82ca58580b1cd7dac
Varun Saini <varunsaini1203@gmail.com>
2020-05-17 20:19:52 +0530

    Smoke Test

---
+++ election/contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Election {
+    // Store candidate
+    // Read candidate
+    string public candidate;
+    // Constructor
+
+    constructor () public {
+        candidate = "Candidate 1";
+    }
+}
